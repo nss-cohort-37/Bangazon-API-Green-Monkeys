be59031e419d88eebff4440f0fc500803ab79b93
jamesnitz <jamesanitz2@gmail.com>
2020-04-02 11:35:10 -0500

    Get all payment types functioning.

---
+++ BangazonAPI/Controllers/PaymentTypeController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BangazonAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+
+namespace BangazonAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PaymentTypesController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+        public PaymentTypesController(IConfiguration config)
+        {
+            _config = config;
+        }
+        public SqlConnection Connection
+        {
+            get
+            {
+                return new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            }
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText =
+                     @"Select p.Id, p.Name, p.Active 
+                     FROM PaymentType p";
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    List<PaymentType> paymentTypes = new List<PaymentType>();
+
+                    while (reader.Read())
+                    {
+                        PaymentType paymentType = new PaymentType
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            Name = reader.GetString(reader.GetOrdinal("Name")),
+                            Active = reader.GetBoolean(reader.GetOrdinal("Active"))
+                        };
+                        paymentTypes.Add(paymentType);
+                    }
+                    reader.Close();
+                    return Ok(paymentTypes);
+                }
+            }
+        }
+    }
+}
